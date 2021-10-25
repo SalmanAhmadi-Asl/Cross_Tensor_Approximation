afabf3a2b8654f7aa3e4be9516cd9ccba77457a7
SalmanAhmadi-Asl <93009466+SalmanAhmadi-Asl@users.noreply.github.com>
2021-10-25 20:45:40 +0300

    Update CUR_matrixF.m

--- CTA_functions/CUR_matrixF.m
+++ CTA_functions/CUR_matrixF.m
@@ -1,12 +1,17 @@
-function [output]=CURT(X,R1,R2)
+function [output]=CUR_matrixF(X,R1,R2)
+% This is the implementation of the matrix CUR approximation, 
+% whose middle matrix U is defined as the intersection matrix of 
+% selected columns and rows. The columns and rows are sampled 
+% uniformly without replacement.
+
 X=double(X);
 N=size(X);
 I=randsample(N(2),R1);
 J=randsample(N(1),R2);
 C=X(:,I);
 R=X(J,:);
-% U=pinv(X(J,I));
-U=pinv(C)*X*pinv(R);
+U=pinv(X(J,I));
+
 output=C*U*R;
-%  size(output)
+
 end
