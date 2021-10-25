5c4465a964a2fba6654e9689a3b3d5dbb9164265
SalmanAhmadi-Asl <93009466+SalmanAhmadi-Asl@users.noreply.github.com>
2021-10-25 20:36:10 +0300

    Update CUR+matrix.m

--- CTA_functions/CUR+matrix.m
+++ CTA_functions/CUR+matrix.m
@@ -1,12 +1,15 @@
-function [output]=CURT(X,R1,R2)
+function [output]=CUR_matrix(X,R1,R2)
+
+%implementation of the matrix CUR approximation with optimal middle matrix (U)
+%The columns and rows are sampled are uniformly without replacement.
+%Other probability distributions can be also used.
 X=double(X);
 N=size(X);
 I=randsample(N(2),R1);
 J=randsample(N(1),R2);
 C=X(:,I);
 R=X(J,:);
-% U=pinv(X(J,I));
+
 U=pinv(C)*X*pinv(R);
 output=C*U*R;
-%  size(output)
 end
